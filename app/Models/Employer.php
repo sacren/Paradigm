7fbf77dd8b28bcf212bb724463ad03427b355852
Jean Sacren <sakiwit@gmail.com>
2024-12-23 23:10:39 -0700

    Add relationship between Job and Employer models
    Job instance belongs to Employer instance and Employer has many Job
    instances.  The belongsTo relationship extends from Job to Employer
    while the hasMany relationship from Employer to Job.
    
    We can manifest these relationships via tinker.
    
    > Employer::find(7)->jobs
    = Illuminate\Database\Eloquent\Collection {#5228
        all: [
          App\Models\Job {#5248
            id: 12,
            employer_id: 7,
            title: "Captain",
            salary: "$55,810.00",
            created_at: "2024-12-24 03:15:20",
            updated_at: "2024-12-24 03:15:20",
          },
          App\Models\Job {#6243
            id: 15,
            employer_id: 7,
            title: "Operations Research Analyst",
            salary: "$59,977.00",
            created_at: "2024-12-24 03:15:20",
            updated_at: "2024-12-24 03:15:20",
          },
        ],
      }
    
    > Job::find(12)->employer
    = App\Models\Employer {#6338
        id: 7,
        name: "Kessler Inc",
        created_at: "2024-12-24 03:15:20",
        updated_at: "2024-12-24 03:15:20",
      }
    
    > Job::find(15)->employer
    = App\Models\Employer {#6241
        id: 7,
        name: "Kessler Inc",
        created_at: "2024-12-24 03:15:20",
        updated_at: "2024-12-24 03:15:20",
      }

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the jobs owned by the employer.
+     *
+     * @return HasMany relationship
+     */
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -15,4 +16,14 @@ class Job extends Model
         'title',
         'salary',
     ];
+
+    /**
+     * Get the employer that owns the job.
+     *
+     * @return BelongsTo relationship
+     */
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
