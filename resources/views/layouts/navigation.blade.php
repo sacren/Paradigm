43e453ae04c3a22ebda6351f633d9ec900b9292c
Jean Sacren <sakiwit@gmail.com>
2025-01-04 22:32:28 -0700

    Add Jobs link in responsive-nav-links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('jobs.index')" :active="request()->routeIs('jobs.index')">
+                {{ __('Jobs') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
