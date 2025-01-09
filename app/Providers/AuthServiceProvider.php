59ec1bc97f9c524b5f1f51e21727f5e159e538e1
Jean Sacren <sakiwit@gmail.com>
2025-01-09 16:05:28 -0700

    Use JobPolicy action explicitly
    Add AuthServiceProvider scaffolding using make:provider.
    
    $ sail artisan make:provider -- AuthServiceProvider
    
       INFO  Provider [app/Providers/AuthServiceProvider.php] created successfully.
    
    Build $policies property of mapping Job model to JobPolicy.  The
    $policies is the default name of the policy.
    
    Update the provider with AuthServiceProvider.
    
    Laravel implements policy implicitly while we do it explicitly.

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Job;
+use App\Policies\JobPolicy;
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Job::class => JobPolicy::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
